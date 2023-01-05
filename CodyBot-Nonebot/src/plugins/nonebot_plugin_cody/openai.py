48293b65454d43a72235b6fe232b531aef4219bc
Icy Cloud <i2cy@outlook.com>
2023-01-05 21:34:50 +0800

    updated name correction in openai API in stop sequence keywords

--- CodyBot-Nonebot/src/plugins/nonebot_plugin_cody/openai.py
+++ CodyBot-Nonebot/src/plugins/nonebot_plugin_cody/openai.py
@@ -11,28 +11,26 @@ from typing import Awaitable
 import openai
 from .config import gpt3_max_tokens
 
-response_sequence_header = "\nCody: "
-human_input_sequence_header = "\nHuman: "
+response_sequence_header = "\nCody:"
+human_input_sequence_header = "\nHuman:"
 
 
-def get_chat_response(key, msg) -> tuple:
+def get_chat_response(key, msg, name) -> tuple:
     openai.api_key = key
     try:
         response: dict = openai.Completion.create(
             model="text-davinci-003",
             prompt=msg,
-            temperature=0.6,
+            temperature=0.7,
             max_tokens=gpt3_max_tokens,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0.6,
-            stop=[" Human:", " Cody:"]
+            stop=["{}:".format(name)]
         )
         res = response['choices'][0]['text'].strip()
-        if response_sequence_header[1:-1] in res:
-            res = res.split(response_sequence_header[1:-1])[-1]
-        elif response_sequence_header[1:-2] + "：" in res:
-            res = res.split(response_sequence_header[1:-2] + "：")[-1]
+        if response_sequence_header[1:] in res:
+            res = res.split(response_sequence_header[1:])[-1]
         while len(res) and res[0] == " ":
             res = res[1:]
         return res, True
